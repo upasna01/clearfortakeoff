39e3001f69a46c4c3c267121d10eae96c92fdfaf
bijaya.kuikel <bijaya.kuikel@yipl.com.np>
2016-04-23 17:16:52 +0545

    state and cities

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -82,7 +82,56 @@
                     <label for="arrival_state">Your Arrival State</label>
                     <select class="form-control" name="arrival_state">
                         <option value="">Arrival State</option>
-                        <option value="boston">Boston</option>
+                        <option value="AL">Alabama </option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona </option>
+                        <option value="AR">Arkansas </option>
+                        <option value="CA">California </option>
+                        <option value="CO">Colorado </option>
+                        <option value="CT">Connecticut </option>
+                        <option value="DE">Delaware </option>
+                        <option value="FL">Florida </option>
+                        <option value="GA">Georgia </option>
+                        <option value="HI">Hawaii </option>
+                        <option value="ID">Idaho </option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
                     </select>
                 </div>
 
@@ -90,7 +139,57 @@
                     <label for="arrival_city">Your Arrival City</label>
                     <select class="form-control" name="arrival_city">
                         <option value="">Arrival City</option>
-                        <option value="lia">Logan International Airport</option>
+                        <option value="Montgomery">Montgomery</option>
+                        <option value="Juneau">Juneau</option>
+                        <option value="Phoenix">Phoenix</option>
+                        <option value="Little_Rock">Little Rock</option>
+                        <option value="Sacramento">Sacramento</option>
+                        <option value="Denver">Denver</option>
+                        <option value="Hartford">Hartford</option>
+                        <option value="Dover">Dover</option>
+                        <option value="Tallahassee">Tallahassee</option>
+                        <option value="Atlanta">Atlanta</option>
+                        <option value="Honolulu">Honolulu</option>
+                        <option value="Boise">Boise</option>
+                        <option value="Springfield">Springfield</option>
+                        <option value="Indianapolis">Indianapolis</option>
+                        <option value="Des_Moines">Des Moines</option>
+                        <option value="Topeka">Topeka</option>
+                        <option value="Frankfort">Frankfort</option>
+                        <option value="Baton_Rouge">Baton Rouge</option>
+                        <option value="Augusta">Augusta</option>
+                        <option value="Annapolis">Annapolis</option>
+                        <option value="Boston">Boston</option>
+                        <option value="Lansing">Lansing</option>
+                        <option value="St. Paul">St. Paul</option>
+                        <option value="Jackson">Jackson</option>
+                        <option value="Jefferson_City">Jefferson City</option>
+                        <option value="Helena">Helena</option>
+                        <option value="Lincoln">Lincoln</option>
+                        <option value="Carson_City">Carson City</option>
+                        <option value="Concord">Concord</option>
+                        <option value="Trenton">Trenton</option>
+                        <option value="Santa_Fe">Santa Fe</option>
+                        <option value="Albany">Albany</option>
+                        <option value="Raleigh">Raleigh</option>
+                        <option value="Bismarck">Bismarck</option>
+                        <option value="Columbus">Columbus</option>
+                        <option value="Oklahoma_City">Oklahoma City</option>
+                        <option value="Salem">Salem</option>
+                        <option value="Harrisburg">Harrisburg</option>
+                        <option value="Providence">Providence</option>
+                        <option value="Columbia">Columbia</option>
+                        <option value="Pierre">Pierre</option>
+                        <option value="Nashville">Nashville</option>
+                        <option value="Austin">Austin</option>
+                        <option value="Salt_Lake_City">Salt Lake City</option>
+                        <option value="Montpelier">Montpelier</option>
+                        <option value="Richmond">Richmond</option>
+                        <option value="Olympia">Olympia</option>
+                        <option value="Charleston">Charleston</option>
+                        <option value="Madison">Madison</option>
+                        <option value="Cheyenne">Cheyenne</option>
+
                     </select>
                 </div>
 
